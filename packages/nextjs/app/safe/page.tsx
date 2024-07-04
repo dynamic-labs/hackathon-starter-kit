66af74f5148bd5cc757ba4b17d65fbad6ece90c6
lim0n3 <simonestaffa96@gmail.com>
2024-07-04 15:42:04 +0200

    safe page

--- packages/nextjs/app/safe/page.tsx
+++ packages/nextjs/app/safe/page.tsx
@@ -1,41 +1,55 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-/* eslint-disable prettier/prettier */
-// app/safe/index.tsx
 "use client";
 
-import { useDynamicContext} from "@dynamic-labs/sdk-react-core";
-import { createWalletClientFromWallet } from "@dynamic-labs/viem-utils";
 import { useState } from "react";
-import { useAccount } from "wagmi";
-import { useEthersV5Provider } from "~~/hooks/scaffold-eth/use-ethers-v5-provider";
-import { useEthersV5Signer } from "~~/hooks/scaffold-eth/use-ethers-v5-signer";
+import { ExternalLinkIcon, useDynamicContext } from "@dynamic-labs/sdk-react-core";
+import { createWalletClientFromWallet } from "@dynamic-labs/viem-utils";
+import { formatUnits } from "viem";
+import { baseSepolia } from "viem/chains";
+import { useAccount, useBalance, useReadContract } from "wagmi";
+import { CheckBadgeIcon, ClipboardIcon } from "@heroicons/react/24/outline";
+import { USDC_ADDRESS } from "~~/lib/constants";
+import { ERC20_ABI } from "~~/lib/erc20_abi";
 import { getPimlicoSmartAccountClient, transferERC20 } from "~~/lib/permissionless";
 
 const SafePage = () => {
   const { address, chain } = useAccount();
-
-  const provider = useEthersV5Provider({ chainId: chain?.id }); //public client
-  const signer = useEthersV5Signer({ chainId: chain?.id }); //wallet client
-  const {primaryWallet} = useDynamicContext();
-  console.log(primaryWallet, "primaryWallet");
+  const { primaryWallet } = useDynamicContext();
 
   const [safeDeployed, setSafeDeployed] = useState(false);
-  const [safeAddress] = useState<string | null>(null);
+  // const [safeAddress, setSafeAddress] = useState<string | null>("0xE65D283F5b62E0ea8e41de5480599655D26293d6");
+  const [safeAddress, setSafeAddress] = useState<string | null>("");
+
+  const [transactions, setTransactions] = useState<string[]>([]); // [txHash, txHash, ...
   const [loading, setLoading] = useState(false);
+  const [transferAmount, setTransferAmount] = useState<number>(0);
+  // const [crossChainTransferAmount, setCrossChainTransferAmount] = useState<number>(0);
+  const [transferTokenAddress, setTransferTokenAddress] = useState<string>("");
+  const [recipientAddress, setRecipientAddress] = useState<string>("");
   const [error, setError] = useState<string | null>(null);
+  const { data: safeBalance, refetch: refetchSafeBalance } = useBalance({
+    address: (safeAddress || ("" as `0x${string}`)) as `0x${string}`,
+    chainId: chain?.id,
+  });
+
+  const { data: safeUSDCBalance, refetch: refetchSafeUSDCBalance } = useReadContract({
+    abi: ERC20_ABI,
+    address: chain ? USDC_ADDRESS[chain?.id] : ("" as `0x${string}`),
+    functionName: "balanceOf",
+    args: [safeAddress],
+  });
 
   const handleDeploySafe = async () => {
     setLoading(true);
     setError(null);
     try {
-      const userAddress = address! as `0x${string}`;
-      if (!primaryWallet) return;
+      const userAddress = address as `0x${string}`;
+      if (!primaryWallet || !chain) return;
       const walletClient = await createWalletClientFromWallet(primaryWallet);
-      await getPimlicoSmartAccountClient(userAddress, chain!, walletClient); //TODO
+      const { account } = await getPimlicoSmartAccountClient(userAddress, chain, walletClient);
+      setSafeAddress(account.address);
       setSafeDeployed(true);
+      refetchSafeBalance();
+      refetchSafeUSDCBalance();
     } catch (err) {
       setError("Failed to deploy Safe account.");
       console.error(err);
@@ -43,24 +57,38 @@ const SafePage = () => {
       setLoading(false);
     }
   };
+
+  /*const depositOnSafe = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      if (!primaryWallet || !chain) return;
+
+      refetchSafeBalance();
+    } catch (err) {
+      setError("Failed to deposit on Safe account.");
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };*/
+
   const handleERC20Transfer = async () => {
     setLoading(true);
     setError(null);
     try {
-      const userAddress = address! as `0x${string}`;
-      if (!primaryWallet) return;
+      const userAddress = address as `0x${string}`;
+      if (!primaryWallet || !chain) return;
       const walletClient = await createWalletClientFromWallet(primaryWallet);
-      console.log(walletClient, "walletClient");
-      const smartAccountClient = await getPimlicoSmartAccountClient(userAddress, chain!, walletClient); 
-      console.log(smartAccountClient, "smartAccountClient");
+      const smartAccountClient = await getPimlicoSmartAccountClient(userAddress, chain, walletClient);
       const txHash = await transferERC20(
-        "0x036CbD53842c5426634e7929541eC2318f3dCF7e", //TODO: change
+        transferTokenAddress,
         smartAccountClient,
-        BigInt(1* 10 ** 2), //TODO: change
-        chain!,
-        "0xC103F2847Ed5EDBFF2666d19CabEd49fF0d3821a" //TODO: change
+        BigInt(transferAmount * 10 ** 6),
+        chain,
+        recipientAddress,
       );
-      console.log("Transfer successful. Transaction hash:", txHash);
+      setTransactions([...transactions, txHash]);
     } catch (err) {
       setError("Failed to transfer tokens.");
       console.error(err);
@@ -69,35 +97,171 @@ const SafePage = () => {
     }
   };
 
+  const canTransfer = transferAmount > 0 && transferTokenAddress && recipientAddress;
+
+  const copyToClipboard = (s: string) => {
+    navigator.clipboard.writeText(s);
+  };
+
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-8">Safe Smart Wallet</h1>
-        <p className="text-lg mb-8">This is your Safe Smart Wallet page.</p>
-        {safeDeployed ? (
-          <>
-            <p>Safe account already deployed!</p>
-            {safeAddress && <p>Your Safe Address: {safeAddress}</p>}
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-              onClick={handleERC20Transfer}
-            >
-              Clickable Only If Safe Deployed
+    <div className="flex flex-col items-center h-screen gap-8 p-12">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold">Safe Smart Wallet</h1>
+        <p className="text-lg">
+          This example deploys a <span className="font-semibold">Safe Smart Wallet</span> and then executes gasless
+          transactions using <span className="font-semibold">Pimlico</span>&apos;s paymaster.
+        </p>
+        <div className="flex flex-row gap-2">
+          <a href="https://docs.safe.global/home/what-is-safe" target="_blank">
+            <button className="btn btn-neutral btn-outline btn-sm">
+              Learn more about Safe
+              <ExternalLinkIcon />
             </button>
-          </>
-        ) : (
-          <>
-            <button
-              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-              onClick={handleDeploySafe}
-              disabled={loading}
-            >
-              {loading ? "Deploying..." : "Deploy Safe Smart Wallet"}
+          </a>
+          <a href="https://docs.pimlico.io/" target="_blank">
+            <button className="btn btn-neutral btn-outline btn-sm">
+              Learn more about Pimlico
+              <ExternalLinkIcon />
             </button>
-            {error && <p className="text-red-500 mt-4">{error}</p>}
-          </>
-        )}
+          </a>
+        </div>
       </div>
+
+      {safeDeployed ? (
+        <div className="flex flex-col justify-center items-center gap-4">
+          <div role="alert" className="alert w-fit border border-white">
+            <div className="flex flex-row items-center gap-2">
+              <CheckBadgeIcon className="w-12 h-12" />
+              <div>
+                <h3 className="font-bold">Safe Smart Wallet deployed!</h3>
+                <div>Address: {safeAddress}</div>
+                {safeBalance && <div>Balance ETH: {formatUnits(safeBalance.value, safeBalance.decimals)} $ETH</div>}
+                {(safeUSDCBalance as bigint) >= 0n && (
+                  <div>Balance USDC: {formatUnits(safeUSDCBalance as bigint, 6)} $USDC</div>
+                )}
+              </div>
+            </div>
+          </div>
+          <div role="tablist" className="tabs tabs-bordered w-full">
+            <input type="radio" name="my_tabs_1" role="tab" className="tab w-full" aria-label="ERC20 Transfer" />
+            <div role="tabpanel" className="tab-content p-10 w-full">
+              <div className="flex flex-col justify-center items-center gap-4">
+                <div className="flex flex-col gap-4 items-center justify-center w-full">
+                  <div className="btn btn-outline btn-xs" onClick={() => copyToClipboard(USDC_ADDRESS[chain!.id])}>
+                    <ClipboardIcon className="w-4 h-4" />
+                    USDC Address
+                  </div>
+                  <label className="input input-bordered flex items-center gap-2 w-full">
+                    <span className="font-medium">Token Address</span>
+                    <input
+                      type="text"
+                      className="grow bg-transparent"
+                      placeholder="0x1aB..."
+                      value={transferTokenAddress}
+                      onChange={e => setTransferTokenAddress(e.target.value)}
+                    />
+                  </label>
+                  <label className="input input-bordered flex items-center gap-2 w-full">
+                    <span className="font-medium">Amount</span>
+                    <input
+                      type="number"
+                      className="grow bg-transparent"
+                      placeholder="0"
+                      value={transferAmount}
+                      onChange={e => setTransferAmount(Number(e.target.value))}
+                    />
+                  </label>
+                  <label className="input input-bordered flex items-center gap-2 w-full">
+                    <span className="font-medium">Recipient Address</span>
+                    <input
+                      type="text"
+                      className="grow bg-transparent"
+                      placeholder="0x1aB..."
+                      value={recipientAddress}
+                      onChange={e => setRecipientAddress(e.target.value)}
+                    />
+                  </label>
+                  <div className="flex flex-col gap-1">
+                    <button className="btn btn-success" onClick={handleERC20Transfer} disabled={!canTransfer}>
+                      Send Transaction
+                    </button>
+                    <p className="text-warning text-xs">
+                      Make sure to have enough balance in the Safe account and the recipient address is valid.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <input
+              type="radio"
+              name="my_tabs_1"
+              role="tab"
+              className="tab  w-full"
+              aria-label="Crosschain USDC Transfer"
+            />
+            <div role="tabpanel" className="tab-content p-10  w-full">
+              <div className="flex flex-col justify-center items-center gap-4">
+                <div className="flex flex-col gap-4 items-center justify-center w-full">
+                  <p className="text-warning text-xs">
+                    This crosschain transfer supports only USDC from Base Sepolia to Arbitrum Sepolia
+                  </p>
+                  <label className="input input-bordered flex items-center gap-2 w-full">
+                    <span className="font-medium">USDC Address</span>
+                    <input
+                      type="text"
+                      className="grow bg-transparent"
+                      placeholder="0x1aB..."
+                      value={USDC_ADDRESS[baseSepolia.id]}
+                      disabled={true}
+                    />
+                  </label>
+                  <label className="input input-bordered flex items-center gap-2 w-full">
+                    <span className="font-medium">Amount</span>
+                    <input
+                      type="number"
+                      className="grow bg-transparent"
+                      placeholder="0"
+                      value={transferAmount}
+                      onChange={e => setTransferAmount(Number(e.target.value))}
+                    />
+                  </label>
+                  <label className="input input-bordered flex items-center gap-2 w-full">
+                    <span className="font-medium">Recipient Address</span>
+                    <input
+                      type="text"
+                      className="grow bg-transparent"
+                      placeholder="0x1aB..."
+                      value={recipientAddress}
+                      onChange={e => setRecipientAddress(e.target.value)}
+                    />
+                  </label>
+                  <div className="flex flex-col gap-1">
+                    <button className="btn btn-success" onClick={handleERC20Transfer} disabled={!canTransfer}>
+                      Send Crosschain Transaction
+                    </button>
+                    <p className="text-warning text-xs">
+                      Make sure to have enough balance in the Safe account and the recipient address is valid.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <button className="btn btn-success" onClick={handleDeploySafe} disabled={loading}>
+            {loading ? (
+              <>
+                <span className="loading loading-spinner"></span>Deploying...
+              </>
+            ) : (
+              "Deploy Safe Account"
+            )}
+          </button>
+          {error && <p className="text-red-500 mt-4">{error}</p>}
+        </>
+      )}
     </div>
   );
 };
---
+++ packages/nextjs/lib/constants.ts
@@ -0,0 +1,6 @@
+import { arbitrumSepolia, baseSepolia } from "viem/chains";
+
+export const USDC_ADDRESS: { [chainId: number]: `0x${string}` } = {
+  [baseSepolia.id]: "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
+  [arbitrumSepolia.id]: "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d",
+};
