6da0d3d82a1e2d99ba75e5709504903c6fbefb33
Matthew Foyle <mfoyle93@gmail.com>
2024-07-09 16:57:51 +0200

    fix: transaction with unknown networks

--- packages/nextjs/lib/dynamic.ts
+++ packages/nextjs/lib/dynamic.ts
@@ -1,45 +1,33 @@
-import { getChain } from "@dynamic-labs/viem-utils";
+import { Wallet } from "@dynamic-labs/sdk-react-core";
+import { getOrMapViemChain } from "@dynamic-labs/viem-utils";
 import { Account, Chain, Hex, Transport, WalletClient, parseEther } from "viem";
+import { customEvmNetworks } from "~~/lib/networks";
 
 export const signMessage = async (message: string, wallet: any): Promise<string> => {
-  if (!wallet) {
-    throw new Error("No wallet found");
-  }
-
   const connector = wallet?.connector;
-
-  if (!connector) {
-    throw new Error("No connector found");
-  }
 
   return await connector.signMessage(message);
 };
 
-export const sendTransaction = async (address: any, amount: string, wallet: any): Promise<string> => {
-  if (!wallet) {
-    throw new Error("No wallet found");
+export const sendTransaction = async (address: any, amount: string, wallet: Wallet): Promise<string> => {
+  const walletClient = wallet.connector.getWalletClient<WalletClient<Transport, Chain, Account>>();
+
+  const chainID = await wallet.connector.getNetwork();
+  const currentNetwork = customEvmNetworks.find(network => network.chainId === chainID);
+
+  if (!currentNetwork) {
+    throw new Error("No chain ID found");
   }
 
-  const connector = wallet?.connector;
-
-  if (!connector) {
-    return "No connector found";
-  }
-
-  const provider: WalletClient<Transport, Chain, Account> = await wallet.connector.getSigner();
-
-  if (!provider) {
-    return "No provider found";
-  }
+  const chain = getOrMapViemChain(currentNetwork);
 
   const transaction = {
     account: wallet.address as Hex,
-    chain: getChain(await provider.getChainId()),
     to: address as Hex,
+    chain,
     value: amount ? parseEther(amount) : undefined,
   };
 
-  const transactionHash = await provider.sendTransaction(transaction);
-
+  const transactionHash = await walletClient.sendTransaction(transaction);
   return transactionHash;
 };
